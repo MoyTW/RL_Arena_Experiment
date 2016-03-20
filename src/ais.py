fe85f1f135d2a7831afee6c8ab0bad394beb8aba
MoyTW <moytravis@gmail.com>
2016-03-20 14:03:17 -0700

    Add throwing item usage to test AI
    Unforutnately the item isn't evicted from the inventory on usage,
    so the guy with the throwing item can kill everybody, but it's
    working - he does throw it!

--- src/ais.py
+++ src/ais.py
@@ -1,3 +1,6 @@
+from src.constants import *
+
+
 class MonsterAI(object):
     def __init__(self, level):
         self.owner = None
@@ -13,12 +16,27 @@ class MonsterAI(object):
 
 class TestMonster(MonsterAI):
     def _take_turn(self):
+
         enemies = self.level.get_objects_outside_faction(self.owner.faction)
+
         if len(enemies) > 0:
+            # Identify the closest enemy
             distances = {self.owner.distance_to(e): e for e in enemies}
             closest_distance = min(distances)
             closest_enemy = distances[closest_distance]
+
+            # Inspect inventory for usable items
+            if self.owner.inventory is not None:
+                usable = self.owner.inventory.get_usable_items()
+                throwing_items = [i for i in usable if i.item.can_use(self.owner, closest_enemy, self.level)]
+            else:
+                throwing_items = []
+
+            # Attack if adjacent
             if closest_distance <= 1.5:
                 self.owner.fighter.attack(closest_enemy)
+            # Throw if you have a throwing item
+            if len(throwing_items) > 0:
+                throwing_items[0].item.use(self.owner, closest_enemy, self.level)
             else:
                 self.owner.move_towards(closest_enemy.x, closest_enemy.y, self.level)
