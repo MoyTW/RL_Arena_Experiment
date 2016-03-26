5a3af067ea6a976808ee29a1fea4a7aa3792df84
MoyTW <moytravis@gmail.com>
2016-03-26 15:40:50 -0700

    Pull run turn out of run level

--- hunting/sim/runner.py
+++ hunting/sim/runner.py
@@ -1,10 +1,13 @@
 from hunting.level.map import LevelMap
 
 
-def run_level(level: LevelMap):
-
-    while len(level.get_factions()) > 1:
-        for o in level._all_objects:
+def run_turn(level: LevelMap):
+    for o in level._all_objects:
             o.ai.take_turn()
+
+
+def run_level(level: LevelMap):
+    while len(level.get_factions()) > 1:
+        run_turn(level)
 
     return level.log
