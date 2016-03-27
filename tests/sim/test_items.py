e0debedf9c99ec998d267587c9974f531b0be686
MoyTW <moytravis@gmail.com>
2016-03-27 16:11:08 -0700

    Tests for Equipment

--- tests/sim/test_items.py
+++ tests/sim/test_items.py
@@ -1,7 +1,44 @@
 import unittest
 from hunting.level.map import LevelMap, LevelTile
-from hunting.sim.entities import GameObject, Fighter
-from hunting.sim.items import Inventory, ThrowingItem
+from hunting.sim.entities import GameObject, Fighter, PropertyEffect
+from hunting.sim.items import Equipment, ThrowingItem, Inventory
+from hunting.constants import *
+
+
+class TestEquipment(unittest.TestCase):
+    def setUp(self):
+        self.level = LevelMap()
+
+        self.test_id = '1'
+        self.test_slot = 'test slot'
+        self.effect = PropertyEffect(property_type=PROPERTY_POWER, value=10)
+        self.sword = GameObject(self.test_id, self.level.log, None, None, 'sword',
+                                item=Equipment(slot=self.test_slot, effects=[self.effect]))
+        self.slot_object = GameObject('9', self.level.log, None, None, 'has slot',
+                                      fighter=Fighter(10, 10, 10, 0, equipment_slots=[self.test_slot]))
+        self.no_slot_object = GameObject('8', self.level.log, None, None, 'does not have slot',
+                                         fighter=Fighter(10, 10, 10, 0, equipment_slots=[]))
+
+    def test_can_equip_and_unequip(self):
+        self.sword.item.use(self.slot_object, self.slot_object, self.level)
+        self.assertTrue(self.sword.item.is_equipped)
+        self.assertEqual({self.test_slot: self.sword}, self.slot_object.fighter.equipment_slots)
+
+        self.sword.item.use(self.slot_object, self.slot_object, self.level)
+        self.assertFalse(self.sword.item.is_equipped)
+        self.assertEqual({self.test_slot: None}, self.slot_object.fighter.equipment_slots)
+
+    def test_cannot_equip_if_slot_occupied(self):
+        slot_holder = GameObject('x', self.level.log, None, None, 'take slot', item=Equipment(self.test_slot, []))
+        slot_holder.item.use(self.slot_object, self.slot_object, self.level)
+        self.assertFalse(self.sword.item.can_use(self.slot_object, self.slot_object, self.level))
+
+    def test_cannot_equip_if_no_matching_slot(self):
+        self.assertFalse(self.sword.item.can_use(self.no_slot_object, self.no_slot_object, self.level))
+
+    def test_applies_effects(self):
+        self.sword.item.use(self.slot_object, self.slot_object, self.level)
+        self.assertEqual(self.slot_object.fighter.power, 20)
 
 
 class TestThrowingItem(unittest.TestCase):
