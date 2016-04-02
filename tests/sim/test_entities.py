6705e973b7cc51b86c4fb0f5cfceeac9c104bc6b
MoyTW <moytravis@gmail.com>
2016-04-02 16:24:51 -0700

    Add test for miss/graze/hit/crit cases on receive_attack
    As a placeholder I just stole PoE's system and values

--- tests/sim/test_entities.py
+++ tests/sim/test_entities.py
@@ -1,4 +1,5 @@
 import unittest
+import unittest.mock as mock
 from hunting.sim.entities import *
 from hunting.level.log import LevelLog
 
@@ -37,6 +38,35 @@ class TestPropertyEffect(unittest.TestCase):
 
 
 class TestFighter(unittest.TestCase):
+    def setUp(self):
+        self.dodger = Fighter(hp=100, defense=0, power=0, xp=0, dodge=0)
+        GameObject('1', LevelLog(), None, None, 'test', fighter=self.dodger)
+
+    @mock.patch('random.randint')
+    def test_receive_attack(self, rand_fn):
+        # Misses
+        rand_fn.return_value = 13
+        self.dodger.receive_attack(0, 10)
+        self.assertEqual(self.dodger.hp, 100)
+
+        # Grazes
+        rand_fn.return_value = 40
+        self.dodger.receive_attack(0, 11)
+        self.assertEqual(self.dodger.hp, 95)
+        self.dodger.hp = 100
+
+        # Hits
+        rand_fn.return_value = 57
+        self.dodger.receive_attack(0, 11)
+        self.assertEqual(self.dodger.hp, 89)
+        self.dodger.hp = 100
+
+        # Crits
+        rand_fn.return_value = 130
+        self.dodger.receive_attack(0, 10)
+        self.assertEqual(self.dodger.hp, 85)
+        self.dodger.hp = 100
+
     def test_minimum_speed_is_one(self):
         self.assertEqual(Fighter(1, 1, 1, 1, base_speed=-5).speed, 1)
         self.assertEqual(Fighter(1, 1, 1, 1, base_speed=0).speed, 1)
