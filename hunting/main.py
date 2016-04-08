d7b61675e8f297fbeaed92a3cf4dd76eb104fe96
MoyTW <moytravis@gmail.com>
2016-04-07 19:26:20 -0700

    Add test server to main

--- hunting/main.py
+++ hunting/main.py
@@ -1,3 +1,6 @@
+import sys
+import http.server
+import socketserver
 import uuid
 
 import tdl
@@ -7,29 +10,59 @@ import hunting.level.encoder as encoder
 from hunting.display.render import Renderer
 from hunting.sim.runner import run_level
 
-print(uuid.uuid4())
+#main_width = 80
+#main_height = 60
+#level_width = 80
+#level_height = 50
 
-main_width = 80
-main_height = 60
-level_width = 80
-level_height = 50
+#main_console = tdl.init(main_width, main_height, 'TDL Test')
 
-main_console = tdl.init(main_width, main_height, 'TDL Test')
+#level = parser.parse_level('resources/test_level.json')
 
-level = parser.parse_level('resources/test_level.json')
+#run_level(level)
 
-run_level(level)
-
-print(level.log.to_json_string())
+#print(level.log.to_json_string())
 
 # This is not very elegant, but the level used for doing the calculations is destructive, so we need to get a fresh
 # from-json copy for rendering.
-scratch_level = parser.parse_level('resources/test_level.json')
+#scratch_level = parser.parse_level('resources/test_level.json')
 
-renderer = Renderer(main_console, level_width, level_height)
-renderer.render_all(level=scratch_level)
+#renderer = Renderer(main_console, level_width, level_height)
+#renderer.render_all(level=scratch_level)
 
-for event in level.log.events:
-    renderer.render_event(level=scratch_level, event=event)
+#for event in level.log.events:
+#    renderer.render_event(level=scratch_level, event=event)
 
-print(encoder.encode_level(level))
+#print(encoder.encode_level(level))
+
+PORT = 8888
+
+serve = True
+
+
+class HelloWorldHandler(http.server.BaseHTTPRequestHandler):
+    def hello_world(self):
+        self.send_response(200)
+        self.wfile.write(bytes('Hello World!', 'utf-8'))
+
+    def goodbye(self):
+        self.send_response(200)
+        self.wfile.write(bytes('Shutting down!', 'utf-8'))
+        print('shutting down')
+        global serve
+        serve = False  # TODO: lol, subclass the server and add this
+
+    def do_GET(self):
+        if self.path == '/goodbye':
+            self.goodbye()
+        else:
+            self.hello_world()
+
+
+Handler = HelloWorldHandler
+
+httpd = http.server.HTTPServer(("", PORT), Handler)
+
+print('starting on port', PORT)
+while serve:  # TODO: subclass server for this
+    httpd.handle_request()
