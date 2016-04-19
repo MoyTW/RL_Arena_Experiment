b5153ba4eaffde6b4922ff0dc5da7245e26d4510
MoyTW <moytravis@gmail.com>
2016-04-18 19:19:40 -0700

    Add pathfinding test

---
+++ tests/level/test_map.py
@@ -0,0 +1,21 @@
+import unittest
+
+from hunting.level.map import LevelTile, LevelMap
+
+
+class TestPathfinding(unittest.TestCase):
+    def test_basic_diagonal(self):
+        level_map = LevelMap()
+        level_map.set_map([[LevelTile() for _ in range(0, 5)] for _ in range(0, 5)])
+
+        self.assertEqual([(1, 1), (2, 2), (3, 3), (4, 4)], level_map.a_star_path(0, 0, 4, 4))
+
+    def test_paths_around_wall(self):
+        level_map = LevelMap()
+        level_map.set_map([[LevelTile() for _ in range(0, 3)] for _ in range(0, 5)])
+
+        for x in range(1, 5):
+            level_map[x][1].blocks = True
+
+        self.assertEqual([(3, 0), (2, 0), (1, 0), (0, 1), (1, 2), (2, 2), (3, 2), (4, 2)],
+                         level_map.a_star_path(4, 0, 4, 2))
