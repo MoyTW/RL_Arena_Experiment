7f4103c710d615695fd62ee551cf78c8565126e5
MoyTW <moytravis@gmail.com>
2016-05-01 14:55:58 -0700

    Reorganize tests to match item module split

--- tests/sim/items/test_equipment.py
+++ tests/sim/items/test_equipment.py
@@ -1,11 +1,9 @@
 import unittest
-import unittest.mock as mock
 
-from hunting.constants import *
-from hunting.level.map import LevelMap, LevelTile
+import hunting.constants as c
 from hunting.sim.effects import PropertyEffect
+from hunting.level.map import LevelMap
 from hunting.sim.entities import GameObject, Fighter
-from hunting.sim.items.core import ThrowingItem, Inventory
 import hunting.sim.items.equipment as equipment
 
 
@@ -15,7 +13,7 @@ class TestEquipment(unittest.TestCase):
 
         self.test_id = '1'
         self.test_slot = 'test slot'
-        self.effect = PropertyEffect(property_type=PROPERTY_POWER, value=10)
+        self.effect = PropertyEffect(property_type=c.PROPERTY_POWER, value=10)
         self.sword = GameObject(self.test_id, self.level.log, None, None, 'sword',
                                 item=equipment.Equipment(slot=self.test_slot, effects=[self.effect]))
         self.slot_object = GameObject('9', self.level.log, None, None, 'has slot',
@@ -49,37 +47,3 @@ class TestEquipment(unittest.TestCase):
         self.sword.item.use(self.slot_object, self.slot_object, self.level)
         self.assertEqual(self.slot_object.fighter.power, 10)
         self.assertEqual(len(self.slot_object.fighter.effect_list), 0)
-
-
-class TestThrowingItem(unittest.TestCase):
-    def setUp(self):
-        self.level = LevelMap()
-        self.level.add_faction('0', {})
-        self.level.set_map([[LevelTile()] for _ in range(0, 5)])
-
-        self.throwing_item = ThrowingItem(1000, 10)
-        self.throwing = GameObject('1', self.level.log, None, None, 'item', item=self.throwing_item)
-        self.inventory = Inventory(10, [self.throwing])
-
-        self.target = GameObject('0', self.level.log, 0, 0, 'test', faction='0',
-                                 fighter=Fighter(9999, 9999, defense=0, power=0, xp=0,
-                                                 death_function=self.level.remove_object))
-        self.level.add_object(self.target)
-
-        self.user = GameObject('0', self.level.log, 4, 0, 'test', faction='1', inventory=self.inventory,
-                               fighter=Fighter(9999, 9999, defense=0, power=0, xp=0,
-                                               death_function=self.level.remove_object))
-
-    def test_is_consumed_on_use(self):
-        self.throwing.item.use(self.user, self.target, self.level)
-        self.assertEqual(0, len(self.inventory))
-
-    @mock.patch('random.randint', return_value=75)
-    def test_does_damage(self, _):
-        self.throwing.item.use(self.user, self.target, self.level)
-        self.assertEqual(8999, self.target.fighter.hp)
-
-    def test_respects_range(self):
-        self.throwing_item.item_range = 1
-        self.throwing.item.use(self.user, self.target, self.level)
-        self.assertEqual(9999, self.target.fighter.hp)
